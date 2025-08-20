1af44f0713eb8c05fe7f172921807589b8b114d3
pnku <159317511+pnk2u@users.noreply.github.com>
2025-08-20 23:31:39 +0200

    Fix image-diff viewer for pixel art

--- app/src/ui/diff/image-diffs/swipe.tsx
+++ app/src/ui/diff/image-diffs/swipe.tsx
@@ -2,9 +2,6 @@ import * as React from 'react'
 import { ICommonImageDiffProperties } from './modified-image-diff'
 import { ImageContainer } from './image-container'
 
-/** How much bigger the slider should be than the images. */
-const SliderOverflow = 14
-
 interface ISwipeState {
   readonly percentage: number
 }
@@ -20,20 +17,33 @@ export class Swipe extends React.Component<
   }
 
   public render() {
+
+    // Figure out if maxSize height or width is bigger
+    const relevantSize =
+      this.props.maxSize.height > this.props.maxSize.width
+        ? this.props.maxSize.height
+        : this.props.maxSize.width;
+
+    const originalWidth = this.props.maxSize.width
+    // Find biggest power of 2 that the relevant size can be multiplied width without exceeding 320
+    const maxPowerOf2 = Math.floor( Math.log2(320 / relevantSize) );
+    const finalHeight = this.props.maxSize.height * Math.pow(2, maxPowerOf2);
+    const finalWidth = this.props.maxSize.width * Math.pow(2, maxPowerOf2);
+
     const style: React.CSSProperties = {
-      height: this.props.maxSize.height,
-      width: this.props.maxSize.width,
+      height: `${finalHeight}px`,
+      width: `${finalWidth}px`,
     }
 
     const swiperWidth =
-      this.props.maxSize.width * (1 - this.state.percentage / 100.0)
+      finalWidth * (1 - this.state.percentage / 100.0)
 
     const previousStyle: React.CSSProperties = {
       position: 'absolute',
       top: 0,
       left: 0,
-      height: this.props.maxSize.height,
-      width: this.props.maxSize.width,
+      height: `${this.props.maxSize.height * Math.pow(2, maxPowerOf2)}px`,
+      width: `${this.props.maxSize.width * Math.pow(2, maxPowerOf2)}px`,
       clipPath: `inset(0 ${Math.floor(swiperWidth)}px 0 0)`,
     }
 
@@ -41,33 +51,41 @@ export class Swipe extends React.Component<
       position: 'absolute',
       top: 0,
       left: 0,
-      height: this.props.maxSize.height,
-      width: this.props.maxSize.width,
+      height: `${this.props.maxSize.height * Math.pow(2, maxPowerOf2)}px`,
+      width: `${this.props.maxSize.width * Math.pow(2, maxPowerOf2)}px`,
       clipPath: `inset(0 0 0 ${Math.floor(
-        this.props.maxSize.width - swiperWidth
+        finalWidth - swiperWidth
       )}px)`,
     }
 
     const maxSize: React.CSSProperties = {
-      maxHeight: this.props.maxSize.height,
-      maxWidth: this.props.maxSize.width,
+      maxHeight: `${finalHeight}px`,
+      maxWidth: `${finalWidth}px`,
+      height: '100%',
+      width: '100%',
     }
 
     return (
       <div className="image-diff-swipe">
         <input
           style={{
-            width: this.props.maxSize.width + SliderOverflow,
+            width: `${finalWidth + Math.pow(2, maxPowerOf2)*2}px`,
+            background: `linear-gradient(to right, var(--color-deleted) ${this.state.percentage}%, var(--color-new) ${this.state.percentage}%)`,
           }}
           className="slider"
           type="range"
           max={100}
           min={0}
           value={this.state.percentage}
-          step={0.1}
+          step={100 / originalWidth}
           onChange={this.onValueChange}
         />
         <div className="sizing-container" ref={this.props.onContainerRef}>
+          <div className="image-container image-container-border" style={{
+            width: `${finalWidth + 2}px`,
+            height: `${finalHeight + 2}px`,
+            background: `linear-gradient(to right, var(--color-deleted) ${this.state.percentage}%, var(--color-new) ${this.state.percentage}%)`,
+          }}></div>
           <div className="image-container" style={style}>
             <div className="image-diff-previous" style={previousStyle}>
               <ImageContainer
